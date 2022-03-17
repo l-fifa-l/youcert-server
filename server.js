e9a6b344f4c8c899a22ee2d13401f591c73cd92a
Vivek <starktestic@gmail.com>
2022-03-17 17:37:49 +0530

    make it better

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// These lines make "require" available
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 import express from 'express';
 import cors from 'cors';
 import { readdirSync } from 'fs';
